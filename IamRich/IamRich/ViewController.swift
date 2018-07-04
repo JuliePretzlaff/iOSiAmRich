28936c68f4493f244b44178183fcd96141eec44c
Julie P <Julie@Austins-iMac.local>
2018-07-03 21:38:44 -0700

    initial commit

---
+++ IamRich/IamRich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Julie P on 7/3/18.
+//  Copyright © 2018 StellarApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
